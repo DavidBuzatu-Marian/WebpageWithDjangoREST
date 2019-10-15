08a4207e073e4b2640569b774f523254be9bd04e
davidbuzatu-marian <davidm.buzatu@gmail.com>
2019-10-15 21:09:57 +0300

    Added Login, Register and Error messages

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -2,13 +2,18 @@ from leads.models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
 
-#Lead Viewset
+# Lead Viewset
 # allows to create a full api without specifying
 # explicit methods.
 
+
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
+    permission_classes = [permissions.IsAuthenticated]
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perfom_create(self, serializer):
+        serializer.save(owner=self.request.user)
